4532f0ccede76462667f4e00e60bed68bef3bb4c
Jonatan Santos <jonatansilvasantos@outlook.com>
2024-08-16 20:33:35 -0300

    recap home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -22,7 +22,7 @@ export function Home() {
                 renderItem={({item}) => (
                     <Group 
                         name={item}
-                        isActive={groupSelected === item }
+                        isActive={groupSelected.toLowerCase() === item.toLowerCase() }
                         onPress={() => setGroupSelected(item)}
                     />
                 )}
