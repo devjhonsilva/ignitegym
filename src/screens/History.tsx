0a1d8d79301035ae94a00974cdd52c13c10f015b
Jonatan Santos <jonatansilvasantos@outlook.com>
2024-08-21 20:58:58 -0300

    Atualização do expo para versão 51.0.28 e atualização do npm. Criação do componente para header e para card na tela de historico

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,9 +1,47 @@
-import { Center, Text } from "@gluestack-ui/themed";
+import { useState } from "react";
+import { SectionList } from "react-native";
+import { HistoryCard } from "@components/HistoryCard";
+import { ScreenHeader } from "@components/ScreenHeader";
+import { Heading, VStack, Text } from "@gluestack-ui/themed";
 
 export function History() {
+    const [exercises, setExercises] = useState([
+        {
+            title: "22.08.2024",
+            data: ["puxada frontal", "remada unilateral"],
+        },
+        {
+            title: "21.08.2024",
+            data: ["puxada frontal"],
+        } 
+    ])
+
     return (
-        <Center flex={1}>
-            <Text>History</Text>
-        </Center>
+        <VStack flex={1}>
+            <ScreenHeader title="Histórico de Exercícios" />
+
+            <SectionList  
+                sections={exercises}
+                keyExtractor={(item) => item}
+                renderItem={() => <HistoryCard />}
+                renderSectionHeader={({section}) => (
+                    <Heading color="$gray200" fontSize="$md" 
+                        mt="$10" mb="$3" fontFamily="$heading">
+                        {section.title}
+                    </Heading>
+                )}
+                style={{paddingHorizontal: 32}}
+                contentContainerStyle={
+                    exercises.length === 0 && {flex: 1, justifyContent: "center"}
+                }
+                ListEmptyComponent={() => (
+                    <Text color="$gray100" textAlign="center">
+                        Não há exercícios registrados ainda. {"\n"}
+                        Vamos Fazer exercícios hoje?
+                    </Text>
+                )}
+                showsVerticalScrollIndicator={false}
+            />
+        </VStack>
     )
 }
