62fc9f3a18a2e7d379d9fccb4d21241144081422
Jonatan Santos <jonatansilvasantos@outlook.com>
2024-09-16 16:10:11 -0300

    criação do contexto para passar os dados do usúario pela aplicação, foi criato a pasta context para conter o arquivo do AuhtContext passando dados e dtos para fazer a tipagem dos dados

--- App.tsx
+++ App.tsx
@@ -6,12 +6,23 @@ import { config } from "./config/gluestack-ui.config";
 import { Loading } from '@components/Loading';
 import { Routes } from '@routes/index';
 
+import { AuthContext } from '@contexts/AuthContexts';
+
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
   return (
     <GluestackUIProvider config={config}>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-      {fontsLoaded ? <Routes /> : <Loading />}
-    </GluestackUIProvider>
+      <AuthContext.Provider value={{
+        user: {
+          id: 'aula',
+          name: 'Jonatan',
+          email: 'jonatan@email.com',
+          avatar: 'jonatan.png'
+        }
+      }}>
+        {fontsLoaded ? <Routes /> : <Loading />}
+      </AuthContext.Provider>
+    </GluestackUIProvider >
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { useContext } from "react";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { AuthRoutes } from "./auth.routes";
 import { AppRoutes } from "./app.routes";
@@ -5,9 +6,13 @@ import { AppRoutes } from "./app.routes";
 import { gluestackUIConfig } from "../../config/gluestack-ui.config"
 import { Box } from "@gluestack-ui/themed";
 
+import { AuthContext } from "@contexts/AuthContexts";
+
 export function Routes() {
     const theme = DefaultTheme
     theme.colors.background = gluestackUIConfig.tokens.colors.gray700
+
+    const contextData = useContext(AuthContext);
 
     return (
         <Box flex={1} bg="$gray600">
